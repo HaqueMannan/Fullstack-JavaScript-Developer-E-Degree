101aa39b8cb595bff03fe476a0168f27ab4eca9a
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2020-07-26 23:37:09 +0100

    TDD in JavaScript - Section 1: Tooling & Introduction to Mocha - Different Types of Mocha and Chai Tests.

--- TDD in JavaScript/Section 1 - Tooling and Introduction to Mocha/intro_to_mocha/tests/calculator-test.js
+++ TDD in JavaScript/Section 1 - Tooling and Introduction to Mocha/intro_to_mocha/tests/calculator-test.js
@@ -5,3 +5,34 @@ describe("Simple assert", function() {
       assert('foo' !== 'bar', 'foo is equal to bar');
    });
 });
+
+describe("Numeric Calculations", function() {
+   it("Addition: Result should be 10", function() {
+      assert.equal(5 + 5, 10);
+   });
+
+   it("Subtract: Result should be 5", function() {
+      assert.equal(10 - 5, 5);
+   });
+
+   it("Less than", function() {
+      assert.isBelow(2, 5, "Number is below 5");
+   });
+});
+
+describe("String Manipulations", function() {
+   it("String should be 'test'", function() {
+      myString = "testing"
+      assert.equal(myString, "test", "The string is not equal to test");
+   });
+
+   it("Variable is a data type string", function() {
+      myString = "testing"
+      assert.typeOf(myString, "string");
+   });
+
+   it("Length is 4'", function() {
+      myString = "test"
+      assert.lengthOf(myString, 4);
+   });
+});
