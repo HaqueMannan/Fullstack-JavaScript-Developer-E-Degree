0f6c1cb93de4e611fc261a4d89e7a9b7619575d4
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2020-08-02 21:02:23 +0100

    JavaScript Frameworks - Section 1: Node & Express - Middleware Functions, Next().

--- JavaScript Frameworks/Node & Express/Node Example/src/index.js
+++ JavaScript Frameworks/Node & Express/Node Example/src/index.js
@@ -1,11 +1,51 @@
 const express = require('express');
 const app = express();
 const hello = require('./hello.js');
+var PORT = process.env.PORT || 3000;
 
 app.get('/hello', function(req,res) {
-   res.send('hello from index.js');
+   res.send(hello);
 });
 
-console.log(hello);
+app.get('/example', function(req, res) {
+   res.send('Hello from the example route');
+});
 
-app.listen(3000);
+app.get('/example/b', function(req, res) {
+   res.send('Hello from sub route B!');
+});
+
+app.get('/example/c', function(req, res) {
+   res.send('Hello from sub route C!');
+});
+
+var callbackOne = function(req, res, next) {
+   console.log('callbackOne');
+   next();
+};
+
+var callbackTwo = function(req, res, next) {
+   console.log('callbackTwo');
+   next();
+};
+
+var callbackThree = function(req, res, next) {
+   console.log('callbackThree says hello from route C!');
+   res.send('callback triggered');
+};
+
+app.get('/example/d', 
+   function(req, res, next) {
+      console.log('The response will be sent by the next function...');
+      next();
+   },
+   function(req, res) {
+      res.send('Hello from D!');
+   }
+);
+
+app.get('/example/c/withmiddleware', [callbackOne, callbackTwo, callbackThree]);
+
+app.listen(PORT, () => {
+   console.log(`Listening on port: ${PORT}`);
+});
