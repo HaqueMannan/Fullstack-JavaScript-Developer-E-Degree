8436eec8418b53a1fc2a6f0dee73e50c2e577496
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2020-08-05 23:59:41 +0100

    JavaScript Frameworks - Section 2: Node & Express RESTful API - Creating a POST Route.

--- JavaScript Frameworks/Node & Express/Section 2 - Node & Rest RESTful API/Express Example/src/index.js
+++ JavaScript Frameworks/Node & Express/Section 2 - Node & Rest RESTful API/Express Example/src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
 const lessons = [
    { id: 1, lesson: 'lesson 1' },
    { id: 2, lesson: 'lesson 2' },
@@ -18,12 +21,21 @@ app.get('/api/lessons/:id', function(req, res) {
    const lesson = lessons.find(function(l) {
       return l.id === parseInt(req.params.id);
    });
-
+   
    if(!lesson) {
       res.status(404).send('The lesson ID provided was not found');
    } else {
       res.send(lesson);
    };
+});
+
+app.post('/api/lessons', function(req, res) {
+   const lesson = {
+      id: lessons.length + 1,
+      lesson: req.body.lesson
+   };
+   lessons.push(lesson);
+   res.send(lesson);
 });
 
 const PORT = process.env.PORT || 3000;
