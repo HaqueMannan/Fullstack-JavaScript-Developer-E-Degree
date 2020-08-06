bc179bcb90182b0b9a938d6cebba8802d7e2623a
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2020-08-06 23:31:20 +0100

    JavaScript Frameworks - Section 2: Node & Express RESTful API - Creating a PUT Route.

--- JavaScript Frameworks/Node & Express/Section 2 - Node & Rest RESTful API/Express Example/src/index.js
+++ JavaScript Frameworks/Node & Express/Section 2 - Node & Rest RESTful API/Express Example/src/index.js
@@ -21,7 +21,7 @@ app.get('/api/lessons/:id', function(req, res) {
    const lesson = lessons.find(function(l) {
       return l.id === parseInt(req.params.id);
    });
-   
+
    if(!lesson) {
       res.status(404).send('The lesson ID provided was not found');
    } else {
@@ -38,6 +38,25 @@ app.post('/api/lessons', function(req, res) {
          lesson: req.body.lesson
       };
       lessons.push(lesson);
+      res.send(lesson);
+   };
+});
+
+app.put('/api/lessons/:id', function(req, res) {
+   // Look up existing lesson
+   const lesson = lessons.find(function(l) {
+      return l.id === parseInt(req.params.id);
+   });
+
+   // If lesson does not exist, return a 404 error - not found
+   if(!lesson) {
+      res.status(404).send('The lesson ID provided was not found');
+   } else if(!req.body.lesson || req.body.lesson.length < 3) {
+      // Validate input, return a 400 error - bad request
+      res.status(400).send('The lesson is required and should be at least 3 characters long');
+   } else {
+      // Update the specified lesson and return updated lesson to client in the browser
+      lesson.lesson = req.body.lesson;
       res.send(lesson);
    };
 });
