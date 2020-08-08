04082535a0f98352d6d3cb833e78216938dd3165
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2020-08-08 16:51:15 +0100

    JavaScript Frameworks - Section 2: Node & Express Mongoose - Mongoose Schema.

--- JavaScript Frameworks/Node & Express/Section 3 - Node & Express Mongoose/Mongoose Example/src/routes/movies.js
+++ JavaScript Frameworks/Node & Express/Section 3 - Node & Express Mongoose/Mongoose Example/src/routes/movies.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('Hello World!'));
+router.get('/:id', (req, res) => req.params.id);
 
 module.exports = router;
