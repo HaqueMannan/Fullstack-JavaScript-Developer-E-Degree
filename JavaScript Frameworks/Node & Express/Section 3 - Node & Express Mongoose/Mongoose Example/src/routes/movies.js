3032c5a545199e0252641d32a3b3148557b149f4
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2020-08-08 17:29:26 +0100

    JavaScript Frameworks - Section 2: Node & Express Mongoose - Mongoose Middleware.

--- JavaScript Frameworks/Node & Express/Section 3 - Node & Express Mongoose/Mongoose Example/src/routes/movies.js
+++ JavaScript Frameworks/Node & Express/Section 3 - Node & Express Mongoose/Mongoose Example/src/routes/movies.js
@@ -14,7 +14,9 @@ router.get('/', async (req, res) => {
    };
 });
 
-router.get('/:id', (req, res) => req.params.id);
+router.get('/:id', getMovie, (req, res) => res.json(res.movie));   // Return the whole object
+// router.get('/:id', getMovie, (req, res) => res.json(res.movie.movieTitle));   // Return single specific property
+// router.get('/:id', getMovie, (req, res) => res.json({ director: res.movie.movieDirector, title: res.movie.movieTitle }));   // Return multiple specific properties as new object
 
 router.post('/', async (req, res) => {
    const movie = new Movie({
@@ -29,6 +31,21 @@ router.post('/', async (req, res) => {
       res.status(400).json({ message: err.message });
    };
 });
+
+// MIDDLEWARE
+async function getMovie(req, res, next) {
+   let movie;
+   try {
+      movie = await Movie.findById(req.params.id);
+      if(movie == null) {
+         return res.status(404).json({ message: 'Cannot find movie.' })
+      };
+   } catch(err) {
+      return res.status(500).json({ message: 'The id selected was not found.' });
+   };
+   res.movie = movie;
+   next();
+};
 
 // EXPORT ROUTER
 module.exports = router;
