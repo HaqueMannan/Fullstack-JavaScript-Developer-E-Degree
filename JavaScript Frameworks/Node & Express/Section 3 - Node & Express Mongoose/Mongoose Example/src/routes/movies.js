2396ce0147e7f50583aa5e31ff14b4dfc4eb3f84
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2020-08-08 17:12:21 +0100

    JavaScript Frameworks - Section 2: Node & Express Mongoose - Post and Get All functions.

--- JavaScript Frameworks/Node & Express/Section 3 - Node & Express Mongoose/Mongoose Example/src/routes/movies.js
+++ JavaScript Frameworks/Node & Express/Section 3 - Node & Express Mongoose/Mongoose Example/src/routes/movies.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Hello World!'));
+// SCHEMAS
+const Movie = require('../models/movie.js');
+
+// ROUTES
+router.get('/', async (req, res) => {
+   try {
+      const movies = await Movie.find();
+      res.json(movies);
+   } catch(err) {
+      res.status(500).json({ message: err.message });
+   };
+});
+
 router.get('/:id', (req, res) => req.params.id);
 
+router.post('/', async (req, res) => {
+   const movie = new Movie({
+      movieDirector: req.body.movieDirector,
+      movieTitle: req.body.movieTitle
+   });
+
+   try{
+      const newMovie = await movie.save();
+      res.status(201).json(newMovie);
+   } catch(err) {
+      res.status(400).json({ message: err.message });
+   };
+});
+
+// EXPORT ROUTER
 module.exports = router;
